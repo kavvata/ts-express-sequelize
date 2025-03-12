49f6396160c0222d4d2b0b21a035307f35cadb43
kavvata <gabriel.kawata@gmail.com>
2025-03-11 23:55:18 -0300

    implementacao de rota incluirServidor

--- src/main/controllers/ServidorController.ts
+++ src/main/controllers/ServidorController.ts
@@ -2,30 +2,41 @@ import { Request, Response } from "express";
 import { Servidor } from "main/database/models/Servidor";
 
 export const listarServidores = async (_req: Request, res: Response) => {
-  const servidores = await Servidor.findAll()
+  const servidores = await Servidor.findAll();
 
   if (servidores.length < 1) {
-    res.send({ servidores: servidores, message: "lista de servidores vazia." })
-    return
+    res.send({ servidores: servidores, message: "lista de servidores vazia." });
+    return;
   }
 
-  res.send({ servidores: servidores })
-}
+  res.send({ servidores: servidores });
+};
 
 export const incluirServidor = async (req: Request, res: Response) => {
-  const { nomeCompleto, email, matricula, cpf } = req.body
+  const { nomeCompleto, email, matricula, cpf } = req.body;
+
+  let existente = await Servidor.findOne({ where: { matricula: matricula } });
+  if (existente) {
+    res.status(403).send({ message: "Erro ao cadastrar servidor: Matrícula já cadastrada" });
+    return;
+  }
+  existente = await Servidor.findOne({ where: { cpf: cpf } });
+  if (existente) {
+    res.status(403).send({ message: "Erro ao cadastrar servidor: CPF já cadastrado" });
+    return;
+  }
 
   try {
     const novoServidor = await Servidor.create({
       nomeCompleto: nomeCompleto,
       email: email,
       matricula: matricula,
-      cpf: cpf
-    })
+      cpf: cpf,
+    });
 
-    res.status(201).send(novoServidor)
+    res.status(201).send(novoServidor);
   } catch (error) {
-    res.status(500).send({ message: "Erro ao cadastrar servidor" })
-    console.log(error)
+    res.status(500).send({ message: "Erro ao cadastrar servidor" });
+    console.log(error);
   }
-}
+};
--- src/tests/integration/TestServidor.spec.ts
+++ src/tests/integration/TestServidor.spec.ts
@@ -3,131 +3,131 @@ import { Servidor, ServidorInstance } from "main/database/models/Servidor";
 import request from "supertest";
 
 describe("Teste entre rota listarServidores e banco de dados", () => {
-  const listaIdsServidor = new Array<number>
+  const listaIdsServidor = new Array<number>();
 
   beforeAll(async () => {
     let servidor = await Servidor.create({
       nomeCompleto: "Joao da Silva",
       email: "joao@teste.com",
       matricula: "1001",
-      cpf: "123.456.789-10"
-    })
-    listaIdsServidor.push(servidor.id)
+      cpf: "123.456.789-10",
+    });
+    listaIdsServidor.push(servidor.id);
 
     servidor = await Servidor.create({
       nomeCompleto: "Maria da Silva",
       email: "maria@teste.com",
       matricula: "1002",
-      cpf: "125.486.780-71"
-    })
-    listaIdsServidor.push(servidor.id)
-  })
+      cpf: "125.486.780-71",
+    });
+    listaIdsServidor.push(servidor.id);
+  });
 
   afterAll(async () => {
-    listaIdsServidor.forEach(async id => {
-      await Servidor.destroy({ where: { id: id } })
-    })
-  })
+    listaIdsServidor.forEach(async (id) => {
+      await Servidor.destroy({ where: { id: id } });
+    });
+  });
 
   it("deve listar todos os servidores com sucesso", async () => {
-    const response = await request(app).get('/servidores')
+    const response = await request(app).get("/servidores");
 
-    expect(response.status).toBe(200)
-    expect(response.body).toHaveProperty('servidores')
-    expect(response.body.servidores).toBeInstanceOf(Array)
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("servidores");
+    expect(response.body.servidores).toBeInstanceOf(Array);
 
     response.body.servidores.forEach((servidor: ServidorInstance) => {
-      expect(listaIdsServidor).toContain(servidor.id)
-    })
-  })
-})
+      expect(listaIdsServidor).toContain(servidor.id);
+    });
+  });
+});
 
 describe("Teste entre rota incluirServidor e banco de dados", () => {
-  let servidorExistenteId: number
-  let servidorId: number
+  let servidorExistenteId: number;
+  let servidorId: number;
 
   afterEach(async () => {
     if (servidorId) {
-      await Servidor.destroy({ where: { id: servidorId } })
+      await Servidor.destroy({ where: { id: servidorId } });
     }
 
     if (servidorExistenteId) {
-      await Servidor.destroy({ where: { id: servidorExistenteId } })
+      await Servidor.destroy({ where: { id: servidorExistenteId } });
     }
-  })
+  });
 
   it("deve cadastrar um novo servidor com sucesso", async () => {
     const servidorDados = {
       nomeCompleto: "Joao da Silva",
       email: "joao@teste.com",
       matricula: "1001",
-      cpf: "123.456.789-10"
-    }
-    const response = await request(app).post('/servidores').send(servidorDados)
+      cpf: "123.456.789-10",
+    };
+    const response = await request(app).post("/servidores").send(servidorDados);
 
-    expect(response.status).toBe(201)
+    expect(response.status).toBe(201);
 
-    expect(response.body).toHaveProperty('id')
-    servidorId = response.body.id as number
+    expect(response.body).toHaveProperty("id");
+    servidorId = response.body.id as number;
 
-    expect(response.body.nomeCompleto as string).toBe(servidorDados.nomeCompleto)
-    expect(response.body.email as string).toBe(servidorDados.email)
-    expect(response.body.matricula as string).toBe(servidorDados.matricula)
-    expect(response.body.cpf as string).toBe(servidorDados.cpf)
-  })
+    expect(response.body.nomeCompleto as string).toBe(servidorDados.nomeCompleto);
+    expect(response.body.email as string).toBe(servidorDados.email);
+    expect(response.body.matricula as string).toBe(servidorDados.matricula);
+    expect(response.body.cpf as string).toBe(servidorDados.cpf);
+  });
 
   it("deve retornar erro ao tentar cadastrar servidor com mesma matricula", async () => {
     const servidorExistenteDados = {
       nomeCompleto: "Maria da Silva",
       email: "maria@teste.com",
       matricula: "1001",
-      cpf: "125.486.780-71"
-    }
+      cpf: "125.486.780-71",
+    };
 
-    const existente = await Servidor.create(servidorExistenteDados)
-    servidorExistenteId = existente.id
+    const existente = await Servidor.create(servidorExistenteDados);
+    servidorExistenteId = existente.id;
 
     const novoServidorDados = {
       nomeCompleto: "Joao da Silva",
       email: "joao@teste.com",
       matricula: "1001",
-      cpf: "123.456.789-10"
-    }
+      cpf: "123.456.789-10",
+    };
 
-    const response = await request(app).post('/servidores').send(novoServidorDados)
+    const response = await request(app).post("/servidores").send(novoServidorDados);
     if (response.body.id) {
-      servidorId = response.body.id
+      servidorId = response.body.id;
     }
-    expect(response.status).toBe(403)
-    expect(response.body).toHaveProperty("message")
-    expect(response.body.message).toBe("Erro ao cadastrar servidor: Matrícula já cadastrada")
-  })
+    expect(response.status).toBe(403);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toBe("Erro ao cadastrar servidor: Matrícula já cadastrada");
+  });
 
   it("deve retornar erro ao tentar cadastrar servidor com mesmo cpf", async () => {
     const servidorExistenteDados = {
       nomeCompleto: "Maria da Silva",
       email: "maria@teste.com",
       matricula: "1002",
-      cpf: "123.456.789-10"
-    }
+      cpf: "123.456.789-10",
+    };
 
-    const existente = await Servidor.create(servidorExistenteDados)
-    servidorExistenteId = existente.id
+    const existente = await Servidor.create(servidorExistenteDados);
+    servidorExistenteId = existente.id;
 
     const novoSerivdorDados = {
       nomeCompleto: "Joao da Silva",
       email: "joao@teste.com",
       matricula: "1001",
-      cpf: "123.456.789-10"
-    }
+      cpf: "123.456.789-10",
+    };
 
-    const response = await request(app).post('/servidores').send(novoSerivdorDados)
+    const response = await request(app).post("/servidores").send(novoSerivdorDados);
     if (response.body.id) {
-      servidorId = response.body.id
+      servidorId = response.body.id;
     }
 
-    expect(response.status).toBe(403)
-    expect(response.body).toHaveProperty("message")
-    expect(response.body.message).toBe("Erro ao cadastrar servidor: CPF já cadastrado")
-  })
-})
+    expect(response.status).toBe(403);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toBe("Erro ao cadastrar servidor: CPF já cadastrado");
+  });
+});
